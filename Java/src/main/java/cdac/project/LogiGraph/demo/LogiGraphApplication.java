1204cc154fb6bea37d984ffb5c344e171dd01300
Mandarhadap <94424096+Mandarhadap@users.noreply.github.com>
2026-01-21 15:17:05 +0530

    Java Base
    Java base to develope on The API 
    PLEASE USE ECLIPSE EE Edition

---
+++ Java/src/main/java/cdac/project/LogiGraph/demo/LogiGraphApplication.java
@@ -0,0 +1,13 @@
+package cdac.project.LogiGraph.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogiGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogiGraphApplication.class, args);
+	}
+
+}
---
+++ Java/src/test/java/cdac/project/LogiGraph/demo/LogiGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package cdac.project.LogiGraph.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogiGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
